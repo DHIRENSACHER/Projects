56d936e20cabef6c764e1882b246791803e46dea
DHIREN SACHER <dhirensacher@gmail.com>
2024-09-09 00:15:50 +0530

    Optimizing number_guessing_game.c

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -1,15 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h.>
+
+int guess1, guess2, guess3, random, level;
+
+void GuessTheNumber()
+{
+    printf("\nFirst guess:");
+    scanf("%d", &guess1);
+    if (guess1 == random)
+    {
+        printf("Congratulations! You got it!");
+    }
+    else
+    {
+        if (guess1 == random + 1 || guess1 == random - 1)
+        {
+            printf("So Close!!\n");
+        }
+        printf("WRONG! Second guess:");
+        scanf("%d", &guess2);
+        if (guess2 == random)
+        {
+            printf("Congratulations! You got it!");
+        }
+        else
+        {
+            if (guess2 == random + 1 || guess2 == random - 1)
+            {
+                printf("So Close!!\n");
+            }
+            printf("WRONG! Last guess:");
+            scanf("%d", &guess3);
+
+            if (guess3 == random)
+            {
+                printf("Congratulations! You got it!");
+            }
+            else
+            {
+                printf("Wrong guess. Game ended");
+                goto end;
+            }
+        }
+    }
+
+end:
+    if ((guess1 != random && guess2 != random && guess3 != random) && level <= 3)
+        printf("\nCorrect answer was:%d", random);
+}
 
 int main()
 {
-
 start:
-
-    srand(time(0));
-    int level, random, guess1, guess2, guess3;
-    printf("\nEnter Level(1 to 3):");
+    printf("Enter difficulty level:");
     scanf("%d", &level);
 
     switch (level)
@@ -17,159 +61,27 @@ start:
     case 1:
         printf("\nA random number is generated between 0-10 and you have 3 tries to guess it\n");
         random = rand() % 10;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            exit;
-        }
-        else
-        {
-            if (guess1 == random + 1 || guess1 == random - 1)
-            {
-                printf("So Close!!\n");
-            }
-
-            printf("WRONG! Second guess:");
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                exit;
-            }
-
-            else
-            {
-                if (guess2 == random + 1 || guess2 == random - 1)
-                {
-                    printf("So Close!!\n");
-                }
-                printf("WRONG! Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    exit;
-                }
-                else
-                {
-                    printf("Wrong guess. Game ended");
-                    goto end;
-                }
-            }
-        }
-
+        GuessTheNumber();
         break;
 
     case 2:
-        printf("\nA random number is generated between 0-50 and you have 3 tries to guess it\n");
-        random = rand() % 50;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            exit;
-        }
-        else
-        {
-            if (guess1 == random + 1 || guess1 == random - 1)
-
-            {
-                printf("So Close!!\n");
-            }
-            printf("WRONG! Second guess:");
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                exit;
-            }
-
-            else
-            {
-                if (guess2 == random + 1 || guess2 == random - 1)
-
-                {
-                    printf("So Close!!\n");
-                }
-                printf("WRONG! Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    exit;
-                }
-                else
-                {
-                    printf("Wrong guess. Game ended");
-                    goto end;
-                }
-            }
-        }
-
+        printf("\nA random number is generated between 0-20 and you have 3 tries to guess it\n");
+        random = rand() % 20;
+        GuessTheNumber();
         break;
 
     case 3:
-        printf("\nA random number is generated between 0-100 and you have 3 tries to guess it\n");
-        random = rand() % 100;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            exit;
-        }
-        else
-        {
-            if (guess1 == random + 1 || guess1 == random - 1)
-            {
-                printf("So Close!!\n");
-            }
-            printf("WRONG! Second guess:");
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                exit;
-            }
-
-            else
-            {
-                if (guess2 == random + 1 || guess2 == random - 1)
-                {
-                    printf("So Close!!\n");
-                }
-                printf("WRONG! Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    exit;
-                }
-                else
-                {
-                    printf("Wrong guess. Game ended");
-                    goto end;
-                }
-            }
-        }
-
+        printf("\nA random number is generated between 0-30 and you have 3 tries to guess it\n");
+        random = rand() % 30;
+        GuessTheNumber();
         break;
 
-    default: 
-        printf("Invalid input!");
-        goto end;
+    default:
+        printf("\nA random number is generated between 0-1000 and you have 3 tries to guess it\n");
+        random = rand() % 1000;
+        GuessTheNumber();
+        break;
     }
-
-end:
-    if ((guess1 != random && guess2 != random && guess3 != random) && level <= 3)
-        printf("\nCorrect answer was:%d", random);
-
     int a;
     printf("\n\nWanna play again? 1:Yes or 0:No --: ");
     scanf("%d", &a);
@@ -182,5 +94,3 @@ end:
 
     return 0;
 }
-
-// hola
