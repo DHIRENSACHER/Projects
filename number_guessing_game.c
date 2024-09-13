8ea7cd739e7f336430f909e0e1c13c49f22a3f05
DHIRENSACHER <dhirensacher@gmail.com>
2024-09-14 04:00:52 +0530

    Revert "Bug fix for number_guessing_game.c"
    This reverts commit 0e056c21354098e9f4e8eadf96e63aaf38f9b9e4.

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -1,15 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h.>
+
+int guess1, guess2, guess3, random, level;
+
+void GuessTheNumber()
+{
+    printf("\nFirst guess:");
+    scanf("%d", &guess1);
+    if (guess1 == random)
+    {
+        printf("Congratulations! You got it!");
+    }
+    else
+    {
+        if (guess1 == random + 1 || guess1 == random - 1)
+        {
+            printf("So Close!!\n");
+        }
+        printf("WRONG! Second guess:");
+        scanf("%d", &guess2);
+        if (guess2 == random)
+        {
+            printf("Congratulations! You got it!");
+        }
+        else
+        {
+            if (guess2 == random + 1 || guess2 == random - 1)
+            {
+                printf("So Close!!\n");
+            }
+            printf("WRONG! Last guess:");
+            scanf("%d", &guess3);
+
+            if (guess3 == random)
+            {
+                printf("Congratulations! You got it!");
+            }
+            else
+            {
+                printf("Wrong guess. Game ended");
+                goto end;
+            }
+        }
+    }
+
+end:
+    if ((guess1 != random && guess2 != random && guess3 != random) && level <= 3)
+        printf("\nCorrect answer was:%d", random);
+}
 
 int main()
 {
-
 start:
-
-    srand(time(0));
-    int level, random, guess1, guess2, guess3;
-    printf("\nEnter Level(1 to 3):");
+    printf("Enter difficulty level:");
     scanf("%d", &level);
 
     switch (level)
@@ -17,220 +61,27 @@ start:
     case 1:
         printf("\nA random number is generated between 0-10 and you have 3 tries to guess it\n");
         random = rand() % 10;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            printf("Correct Answer You won!\n"); exit;
-        }
-        else
-        {   
-            if (guess1 == random + 1 || guess1 == random - 1)
-            {
-                printf("So Close!!\n");
-            }
-
-            printf("\nWRONG!\n");
-            if(guess1<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess1 > random)
-                {
-                    printf("Lower number please\n");
-                }
-            printf("Second guess:");
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                printf("Correct Answer You won!\n"); exit;
-            }
-
-            else
-            {
-                
-                if (guess2 == random + 1 || guess2 == random - 1)
-                {
-                    printf("So Close!!\n");
-                }
-                printf("\nWRONG!\n");
-                if(guess2<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess2 > random)
-                {
-                    printf("Lower number please\n");
-                }
-                printf("Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    printf("Correct Answer You won!\n"); exit;
-                }
-                else
-                {
-                    printf("wrong guess. Game ended\n");
-                    goto end;
-                }
-            }
-        }
-
+        GuessTheNumber();
         break;
 
     case 2:
-        printf("\nA random number is generated between 0-50 and you have 3 tries to guess it\n");
-        random = rand() % 50;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            printf("Correct Answer You won!\n"); exit;
-        }
-        else
-        {
-            
-            if (guess1 == random + 1 || guess1 == random - 1)
-
-            {
-                printf("So Close!!\n");
-            }
-            printf("\nWRONG!\n"); 
-            if(guess1<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess1 > random)
-                {
-                    printf("Lower number please\n");
-                }
-            printf("Second guess:");
-
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                printf("Correct Answer You won!\n"); exit;
-            }
-
-            else
-            {
-                
-                if (guess2 == random + 1 || guess2 == random - 1)
-
-                {
-                    printf("So Close!!\n");
-                }
-                printf("\nWRONG!\n");
-                if(guess2<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess2 > random)
-                {
-                    printf("Lower number please\n");
-                }
-
-                printf("Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    printf("Correct Answer You won!\n"); exit;
-                }
-                else
-                {
-                    printf("wrong guess. Game ended\n");
-                    goto end;
-                }
-            }
-        }
-
+        printf("\nA random number is generated between 0-20 and you have 3 tries to guess it\n");
+        random = rand() % 20;
+        GuessTheNumber();
         break;
 
     case 3:
-        printf("\nA random number is generated between 0-100 and you have 3 tries to guess it\n");
-        random = rand() % 100;
-
-        printf("\nFirst guess:");
-        scanf("%d", &guess1);
-
-        if (guess1 == random)
-        {
-            printf("Correct Answer You won!\n"); exit;
-        }
-        else
-        {
-           
-            if (guess1 == random + 1 || guess1 == random - 1)
-            {
-                printf("So Close!!\n");
-            }
-            printf("\nWRONG!\n");
-             if(guess1<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess1 > random)
-                {
-                    printf("Lower number please\n");
-                }
-            printf("Second guess:");
-            scanf("%d", &guess2);
-
-            if (guess2 == random)
-            {
-                printf("Correct Answer You won!\n"); exit;
-            }
-
-            else
-            {
-                
-                if (guess2 == random + 1 || guess2 == random - 1)
-                {
-                    printf("So Close!!\n");
-                }
-                printf("\nWRONG!\n");
-                if(guess2<random)
-                {
-                    printf("Higher number please\n");
-                }
-                else if(guess2 > random)
-                {
-                    printf("Lower number please\n");
-                }
-                printf("Last guess:");
-                scanf("%d", &guess3);
-
-                if (guess3 == random)
-                {
-                    printf("Correct Answer You won!\n"); exit;
-                }
-                else
-                {
-                    printf("wrong guess. Game ended\n");
-                    goto end;
-                }
-            }
-        }
-
+        printf("\nA random number is generated between 0-30 and you have 3 tries to guess it\n");
+        random = rand() % 30;
+        GuessTheNumber();
         break;
 
-    default: 
-        printf("Invalid input!");
-        goto end;
+    default:
+        printf("\nA random number is generated between 0-1000 and you have 3 tries to guess it\n");
+        random = rand() % 1000;
+        GuessTheNumber();
+        break;
     }
-
-end:
-    if ((guess1 != random && guess2 != random && guess3 != random) && level <= 3)
-        printf("\nCorrect answer was:%d", random);
-
     int a;
     printf("\n\nWanna play again? 1:Yes or 0:No --: ");
     scanf("%d", &a);
@@ -243,5 +94,3 @@ end:
 
     return 0;
 }
-
-// hola
